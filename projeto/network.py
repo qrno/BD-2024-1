ff990bbc5744660c17df7fb891c10081c978d3f8
Eduardo Quirino <eduardo.quirino.oliveira@gmail.com>
2024-09-07 14:14:08 -0300

    Added user list

--- projeto/network.py
+++ projeto/network.py
@@ -1,7 +1,19 @@
-from flask import (Blueprint, flash, g, redirect, render_template, request, url_for)
+from flask import (Blueprint, render_template)
+
+from projeto.db import get_db
+from projeto.auth import login_required
 
 bp = Blueprint('network', __name__)
 
 @bp.route('/')
 def index():
     return render_template('network/index.html')
+
+@bp.route('/user_list')
+@login_required
+def user_list():
+    db = get_db()
+    users = db.execute(
+        "SELECT * FROM user"
+    )
+    return render_template('network/user_list.html', users=users)
