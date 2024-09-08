eca1023fe5c087022404604145defc519046e64d
Diehlgit <arthurdiehlbarroso@gmail.com>
2024-09-08 19:49:33 -0300

    Adicionando as funções da camada de persistencia

--- projeto/db_queries.py
+++ projeto/db_queries.py
@@ -1,5 +1,13 @@
-def get_all_users():
-    users = db.execute(
+from flask import (session)
 
+from projeto.db import get_db
+
+def get_all_users():
+    db = get_db()
+    users = db.execute(
+        "SELECT * FROM user"
     ).fetchall()
     return users
+
+def get_user_id():
+    return session.get('user_id')
